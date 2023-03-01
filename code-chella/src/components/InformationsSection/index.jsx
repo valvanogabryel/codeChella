986dd60bb2b17a85ad576cccd94c1cd15e6598ab
valvanogabryel <valvanogabryel@gmail.com>
2023-03-01 14:08:51 -0300

    ;

--- code-chella/src/components/InformationsSection/index.jsx
+++ code-chella/src/components/InformationsSection/index.jsx
@@ -1,22 +1,22 @@
+import classNames from "classnames";
 import Button from "components/Button";
 import styles from './InformationsSection.module.css';
 
 // images
-import homeImage from 'assets/images/programodromo.png'; // Home page
+// import homeImage from 'assets/images/programodromo.png'; // Home page
 
-const InformationsSection = ({ title, children, hasButton, page, image }) => {
+const InformationsSection = ({ title, children, hasButton, page, id }) => {
   return (
     <section className={styles.informations__container}>
-      {
-        page === 'home' ?
-          <div className={styles.informations__image}>
-            <img src={homeImage} alt="Pessoas assistindo um show" className={styles.image} />
-          </div>
-          :
-          <div className={styles.informations__image}>
-            <img src={image} alt="" className={styles.image} />
-          </div>
-      }
+
+      <div className={classNames({
+        [styles.informations__image]: true,
+        [styles[`informations__image--${page}`]]: true,
+        [styles[`informations__image--${page}-${id}`]]: true
+      })}>
+        {/* <img src={homeImage} alt="Pessoas assistindo um show" className={styles.image} /> */}
+      </div>
+
       <div className={styles.informations__info}>
         <h6 className={styles.informations__title}>{title}</h6>
         <p className={styles.informations__text}>
@@ -31,7 +31,7 @@ const InformationsSection = ({ title, children, hasButton, page, image }) => {
             <></>
         }
       </div>
-    </section>
+    </section >
   );
 }
 
--- code-chella/src/pages/Experience/index.jsx
+++ code-chella/src/pages/Experience/index.jsx
@@ -1,9 +1,7 @@
 import Banner from "components/Banner";
 import InformationsSection from "components/InformationsSection";
 
-import image1 from 'assets/images/experience/image1.png';
-import image2 from 'assets/images/experience/image2.png';
-import image3 from 'assets/images/experience/image3.png';
+
 
 import styles from './Experience.module.css';
 
@@ -15,19 +13,22 @@ const Experience = () => {
         <InformationsSection
           title='Acessibilidade e Inclusão'
           page='experience'
-          image={image1}>
+          id={1}
+        >
           Nosso evento tenta abraçar um público mais amplo em todos os sentidos, de todas as idades, corpos, gostos e pensamentos!  Isso está presente no  espaço físico, na sinalização, no treinamento da equipe de apoio, na comunicação em libras, braile, visual e tátil. Além disso, dispomos de ingressos gratuitos para pessoas com deficiência e acompanhantes, pessoas idosas e crianças!
         </InformationsSection>
         <InformationsSection
           title='Sustentabilidade'
           page='experience'
-          image={image2}>
+          id={2}
+        >
           Queremos estar aqui celebrando daqui a 50, 100 anos! Por isso, levamos o futuro do nosso planeta a sério. Nosso festival é carbono zero, com utilização de copos retornáveis, coleta e destinação adequada de resíduos, e transporte coletivo.
         </InformationsSection>
         <InformationsSection
           title='Atrações'
           page='experience'
-          image={image3}>
+          id={3}
+        >
           Intervenções artísticas e culturais para todos os gostos! Durante os intervalos dos shows, nossa experiência contará com recreação para crianças, oficina de cerâmica, origami, circo e aquarela! Além disso, nosso espaço contra com 3 praças de alimentação, roda gigante e outros brinquedos!
         </InformationsSection>
       </main>
