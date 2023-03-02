d38ae157767b7c7a0226c3ff23d4a199afef2b55
valvanogabryel <valvanogabryel@gmail.com>
2023-03-02 11:00:28 -0300

    ;

--- code-chella/src/pages/Sectors/index.jsx
+++ code-chella/src/pages/Sectors/index.jsx
@@ -1,7 +1,36 @@
 import Banner from "components/Banner";
+import { Fragment } from "react";
 import styles from './Sectors.module.css';
 
 const Sectors = () => {
+  const sectors = [
+    {
+      name: 'Pista Premium',
+      id: 1,
+      color: '#0E7DF1'
+    },
+    {
+      name: 'Pista Comum',
+      id: 2,
+      color: '#FE016E'
+    },
+    {
+      name: 'Cadeiras térreo',
+      id: 3,
+      color: '#01A89E'
+    },
+    {
+      name: 'Cadeiras Superiores',
+      id: 4,
+      color: '#3F51B5'
+    },
+    {
+      name: 'Rampas',
+      id: 5,
+      color: '#0E7DF1'
+    }
+  ];
+
   return (
     <>
       <Banner page='sectors' />
@@ -9,8 +38,26 @@ const Sectors = () => {
         <div className={styles.sectors__image}>
           {/* image */}
         </div>
-        <div>
-          <h6>Legenda:</h6>
+        <div className={styles.sectors__wrapper}>
+          <h6 className={styles.sectors__subtitle}>Legenda:</h6>
+          <ul className={styles.sectors__list}>
+            {
+              sectors.map(sector => (
+                <div
+                  key={sector.id}
+                  className={styles.sector__item}
+                >
+                  <span
+                    style={{ background: sector.color }}
+                    className={styles.sector__box}
+                  ></span>
+                  <li>
+                    {sector.name}
+                  </li>
+                </div>
+              ))
+            }
+          </ul>
         </div>
       </section>
     </>
