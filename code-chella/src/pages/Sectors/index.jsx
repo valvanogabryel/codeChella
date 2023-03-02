09148d1cd8a6a759e0281c44dbb02168883cbcaa
valvanogabryel <valvanogabryel@gmail.com>
2023-03-02 10:28:26 -0300

    ;

--- code-chella/src/pages/Sectors/index.jsx
+++ code-chella/src/pages/Sectors/index.jsx
@@ -1,9 +1,20 @@
+import Banner from "components/Banner";
+import styles from './Sectors.module.css';
+
 const Sectors = () => {
-    return (
-        <div>
-            setores
+  return (
+    <>
+      <Banner page='sectors' />
+      <section className={styles.sectors__container}>
+        <div className={styles.sectors__image}>
+          {/* image */}
         </div>
-    );
+        <div>
+          <h6>Legenda:</h6>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Sectors;
