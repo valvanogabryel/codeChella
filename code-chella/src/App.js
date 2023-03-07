03a959cfef8b3fb3edccc1647429b38527503f0a
valvanogabryel <valvanogabryel@gmail.com>
2023-03-07 11:23:29 -0300

    Update App.js

--- code-chella/src/App.js
+++ code-chella/src/App.js
@@ -1,21 +1,6 @@
-import { useEffect } from 'react';
 import AppRoutes from './routes.js';
 
 const App = () => {
-  // useEffect(() => {
-  //   function preventHorizontalScroll(event) {
-  //     if (event.touches.length > 1) {
-  //       event.preventDefault();
-  //     }
-  //   }
-
-  //   document.body.addEventListener('touchmove', preventHorizontalScroll, { passive: false });
-
-  //   return () => {
-  //     document.body.removeEventListener('touchmove', preventHorizontalScroll);
-  //   }
-  // }, []);
-
   return (
     <AppRoutes />
   );
