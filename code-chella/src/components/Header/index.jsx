c80af2bc99af51eff947fd2ba26b40e260a45eb3
valvanogabryel <valvanogabryel@gmail.com>
2023-03-17 11:39:21 -0300

    Update index.jsx

--- code-chella/src/components/Header/index.jsx
+++ code-chella/src/components/Header/index.jsx
@@ -14,11 +14,6 @@ const Header = () => {
   const [isActive, setMode] = useState(false);
   const location = useLocation();
 
-
-  useEffect(() => {
-
-  });
-
   useEffect(() => {
     setMode(false);
   }, [location]);
